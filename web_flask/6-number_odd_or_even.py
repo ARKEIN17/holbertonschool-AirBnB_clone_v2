5948fabda59ab0bbffa8d8f723d1483285204a95
ARKEIN17 <ramirez1kevin98@gmail.com>
2022-09-23 15:57:51 -0500

    task 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,58 +1,63 @@
 #!/usr/bin/python3
-"""script that starts a Flask web application:"""
+"""FLASK MODULE"""
+
 from flask import Flask, render_template
+flask = Flask(__name__)
 
 
-app = Flask(__name__)
-app.url_map.strict_slashes = False
+@flask.route('/', strict_slashes=False)
+def hello_flask():
+    """String to display"""
+    return 'Hello HBNB!'
 
 
-@app.route("/")
-def hello_route():
-    return "Hello HBNB!"
+@flask.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    """String to display in another route"""
+    return 'HBNB'
 
 
-@app.route("/hbnb")
-def hbnb_route():
-    return "HBNB"
-
-
-@app.route("/c/<text>")
-def c_route(text):
+@flask.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """GET Route /c and <text> to print"""
     text = text.replace("_", " ")
-    return "C " + text
+    return 'C {}'.format(text)
 
 
-@app.route("/python/", defaults={'text': 'is cool'})
-@app.route("/python/<text>")
-def python(text):
+@flask.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text):
+    """Print the text on the route"""
     text = text.replace("_", " ")
-    return "Python " + text
+    return 'Python {}'.format(text)
 
 
-@app.route("/number/<int:n>")
-def number(n):
-    return str(n) + " is a number"
+@flask.route('/python', strict_slashes=False)
+def python_is_default_cool(text='is cool'):
+    """Print the text on the default route"""
+    text = text.replace("_", " ")
+    return 'Python {}'.format(text)
 
 
-@app.route("/number_template/<int:n>")
+@flask.route('/number/<int:n>', strict_slashes=False)
+def is_a_number(n):
+    """Returns string if n is a number."""
+    return '{} is a number'.format(n)
+
+
+@flask.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    return render_template('5-number.html', data=n)
+    """Returns string if n is a number."""
+    return render_template('5-number.html', n=n)
 
 
-@app.route("/number_odd_or_even/<int:n>")
-def number_odd_or_even(n):
-    data = {
-        'number': n,
-        'class': 'odd'
-    }
-    if n % 2 == 0:
-        data = {
-            'number': n,
-            'class': 'even'
-        }
-    return render_template('6-number_odd_or_even.html', data=data)
+@flask.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_template_even(n):
+    """Returns string if n is a number depends on odd or even."""
+    return render_template('6-number_odd_or_even.html', n=n)
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == '__main__':
+    flask.run(
+        host='0.0.0.0',
+        port=5000,
+    )
